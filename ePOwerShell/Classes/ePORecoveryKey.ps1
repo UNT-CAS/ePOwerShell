1d6763f3cf305e5f07e948eb7567a7ddc1957292
Michael Arranaga <michaelarranaga@my.unt.edu>
2019-05-11 12:16:30 -0500

    New class builder

--- ePOwerShell/Classes/ePORecoveryKey.ps1
+++ ePOwerShell/Classes/ePORecoveryKey.ps1
@@ -2,4 +2,10 @@ Class ePORecoveryKey {
     [System.String] $ComputerName
     [System.String] $DriveLetter
     [System.String] $RecoveryKey
+
+    ePORecoveryKey([System.String] $ComputerName, [System.String] $DriveLetter, [System.String] $RecoveryKey) {
+        $this.ComputerName = $ComputerName
+        $this.DriveLetter = $DriveLetter
+        $this.RecoveryKey = $RecoveryKey
+    }
 }
