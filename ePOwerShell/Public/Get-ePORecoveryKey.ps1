773e9cb6b1c0865a49fee513b8bdb7636780eb6d
Michael Arranaga <michaelarranaga@my.unt.edu>
2019-05-11 12:18:45 -0500

    Added pipeline, simplified function, and returns an object now

--- ePOwerShell/Public/Get-ePORecoveryKey.ps1
+++ ePOwerShell/Public/Get-ePORecoveryKey.ps1
@@ -7,45 +7,36 @@
 
     Returns the MNE Recovery Key for specified encrypted systems. If no key is found, throws a warning    
 
-.PARAMETER LeafNodeId
-
-    Specifies the unique Leaf Node ID for each individual computer
-
-.PARAMETER SerialNumber
-
-    Specifies the unique Serial Number for each individual computer
-
 .EXAMPLE
 
-    Get-ePOwerShellMneRecoveryKey '12345'
-
-.EXAMPLE
-
-    Get-ePOwerShellMneRecoveryKey -SerialNumber 'C035406KHV5K'
+    Get-ePORecoveryKey 'My-ComputerName'
 #>
 
 function Get-ePORecoveryKey {
-    [CmdletBinding(DefaultParametersetname = 'ComputerName')]
+    [CmdletBinding(DefaultParametersetname = 'Computer')]
     [Alias('Get-ePOwerShellMneRecoveryKey', 'Get-ePOMneRecoveryKey')]
     [OutputType([System.Object[]])]
     param (
-        [Parameter(Mandatory = $True, ParameterSetName = 'ComputerName', Position = 0, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
+        <#
+            .PARAMETER Computer
+                Specifies either an ePOComputer object or the system name found in ePO
+        #>
+        [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
         [Alias('Name')]
-        [String[]]
-        $ComputerName,
-
-        [Parameter(Mandatory = $True, ParameterSetName = 'LeafNode')]
-        [String[]]
-        $LeafNodeId,
-
-        [Parameter(Mandatory = $True, ParameterSetName = 'SerialNumber')]
-        [String[]]
-        $SerialNumber
+        $Computer
     )
 
     begin {
         try {
             [System.Collections.ArrayList] $Found = @()
+            
+            $Request = @{
+                Name        = 'mne.recoverMachine'
+                Query       = @{
+                    serialNumber = ''
+                }
+                ErrorAction = 'Stop'
+            }
         } catch {
             Write-Information $_ -Tags Exception
             Throw $_
@@ -53,111 +44,37 @@ function Get-ePORecoveryKey {
     }
 
     process {
-        switch ($PSCmdlet.ParameterSetName) {
-            'ComputerName' {
-                if (-not ($Table.Columns | ? { $_.ColumnName -eq 'ComputerName' })) {
-                    $Column1 = New-Object System.Data.DataColumn 'ComputerName', ([String])
-                    [void]$Table.Columns.Add($Column1)
-                }
-
-                if (-not ($Table.Columns | ? { $_.ColumnName -eq 'RecoveryKey' })) {
-                    $Column2 = New-Object System.Data.DataColumn 'RecoveryKey', ([String])
-                    [void]$Table.Columns.Add($Column2)
-                }
-                
-
-                foreach ($Computer in $ComputerName) {
-                    try {
-                        $ComputerInformation = Find-ePOwerShellComputerSystem -ComputerName $Computer
-                    } catch {
-                        Write-Warning ('Failed to find comptuer system in ePO: {0}' -f $Computer)
-                        continue
-                    }
-
-                    $Request = @{
-                        Name     = 'mne.recoverMachine'
-                        PassThru = $True
-                        Query    = @{
-                            epoLeafNodeId = $ComputerInformation.ParentID
-                        }
-                    }
-
-                    try {
-                        $Key = Invoke-ePORequest @Request
-                    } catch {
-                        Write-Warning ('Failed to find detect recovery key for computer: {0}' -f $ComputerInformation.ComputerName)
-                        continue
-                    }
-
-                    $Row = $Table.NewRow()
-                    $Row.ComputerName = $ComputerInformation.ComputerName
-                    $Row.RecoveryKey = $Key
-                    [void]$Table.Rows.Add($Row)
-                }
+        foreach ($Comp in $Computer) {
+            if ($Comp -is [ePOComputer]) {
+                Write-Verbose 'Computer is an ePOComputer object'
+            } else {
+                Write-Verbose 'Computer is not an ePOComputer object. Searching for...'
+                $Comp = Get-ePOComputer -Computer $Comp -ErrorAction Stop
             }
-            'LeafNode' {
-                # if (-not ($Table.Columns | ? { $_.ColumnName -eq 'LeafNode' })) {
-                    $Column1 = New-Object System.Data.DataColumn 'LeafNode', ([String])
-                    [void]$Table.Columns.Add($Column1)
-                # }
 
-                # if (-not ($Table.Columns | ? { $_.ColumnName -eq 'RecoveryKey' })) {
-                    $Column2 = New-Object System.Data.DataColumn 'RecoveryKey', ([String])
-                    [void]$Table.Columns.Add($Column2)
-                # }
-
-                foreach ($LeafNode in $LeafNodeId) {
-                    $Request = @{
-                        Name     = 'mne.recoverMachine'
-                        PassThru = $True
-                        Query    = @{
-                            epoLeafNodeId = $LeafNode
-                        }
+            $QueryRequest = @{
+                Table = 'MneVolumes'
+                Select = @(
+                    'MneFvRecoveryKeys.DisplayName',
+                    'MneVolumes.MountPoint'
+                )
+                Where = @{
+                    eq = @{
+                        'MneVolumes.EPOLeafNodeId' = $Comp.ParentID
                     }
-
-                    try {
-                        $Key = Invoke-ePORequest @Request
-                    } catch {
-                        Throw "Failed to find detect recovery key: $($_.Exception.Message)"
-                    }
-
-                    $Row = $Table.NewRow()
-                    $Row.LeafNode = $LeafNode
-                    $Row.RecoveryKey = $Key
-                    [void]$Table.Rows.Add($Row)
                 }
+                ErrorAction = 'Stop'
             }
-            'SerialNumber' {
-                # if (-not ($Table.Columns | ? { $_.ColumnName -eq 'SerialNumber' })) {
-                    $Column1 = New-Object System.Data.DataColumn 'SerialNumber', ([String])
-                    [void]$Table.Columns.Add($Column1)
-                # }
 
-                # if (-not ($Table.Columns | ? { $_.ColumnName -eq 'RecoveryKey' })) {
-                    $Column2 = New-Object System.Data.DataColumn 'RecoveryKey', ([String])
-                    [void]$Table.Columns.Add($Column2)
-                # }
+            $MountPoints = Invoke-ePOQuery @QueryRequest
 
-                foreach ($SN in $SerialNumber) {
-                    $Request = @{
-                        Name     = 'mne.recoverMachine'
-                        PassThru = $True
-                        Query    = @{
-                            serialNumber = $SN
-                        }
-                    }
+            foreach ($MountPoint in $MountPoints) {
+                Write-Verbose ('Getting recovery key for mount point: {0}' -f $MountPoint.'MneFvRecoveryKeys.DisplayName')
+                $Request.Query.serialNumber = $MountPoint.'MneFvRecoveryKeys.DisplayName'
 
-                    try {
-                        $Key = Invoke-ePORequest @Request
-                    } catch {
-                        Throw "Failed to find detect recovery key: $($_.Exception.Message)"
-                    }
-
-                    $Row = $Table.NewRow()
-                    $Row.SerialNumber = $SN
-                    $Row.RecoveryKey = $Key
-                    [void]$Table.Rows.Add($Row)
-                }
+                $RecoveryKey = Invoke-ePORequest @Request
+                $RecoveryKeyObject = [ePORecoveryKey]::new($Comp.ComputerName, $MountPoint.'MneVolumes.MountPoint', $RecoveryKey)
+                [Void] $Found.Add($RecoveryKeyObject)
             }
         }
     }
