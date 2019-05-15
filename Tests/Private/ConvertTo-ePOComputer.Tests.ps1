44ad6b734ddbb5fc4ce6792a7101cb6f898b5a7d
Michael Arranaga <Michael.Arranaga@unt.edu>
2019-05-15 08:56:42 -0500

    Sharing current

---
+++ Tests/Private/ConvertTo-ePOComputer.Tests.ps1
@@ -0,0 +1,78 @@
+[System.String]    $ProjectDirectoryName = 'ePOwerShell'
+[System.String]    $FunctionType         = 'Private'
+[IO.FileInfo]      $PesterFile           = [IO.FileInfo] ([System.String] (Resolve-Path -Path $MyInvocation.MyCommand.Path))
+[System.String]    $FunctionName         = $PesterFile.Name.Split('.')[0]
+[IO.DirectoryInfo] $ProjectRoot          = Split-Path -Parent $PesterFile.Directory
+
+While (-not ($ProjectRoot.Name -eq $ProjectDirectoryName)) {
+    $ProjectRoot = Split-Path -Parent $ProjectRoot.FullName
+}
+
+[IO.DirectoryInfo] $ProjectDirectory     = Join-Path -Path $ProjectRoot -ChildPath $ProjectDirectoryName -Resolve
+[IO.DirectoryInfo] $PublicDirectory      = Join-Path -Path $ProjectDirectory -ChildPath 'Public' -Resolve 
+[IO.DirectoryInfo] $PrivateDirectory     = Join-Path -Path $ProjectDirectory -ChildPath 'Private' -Resolve 
+[IO.DirectoryInfo] $ExampleDirectory     = Join-Path (Join-Path -Path $ProjectRoot -ChildPath 'Examples' -Resolve) -ChildPath $FunctionType -Resolve
+[IO.DirectoryInfo] $ExampleDirectory     = Join-Path $ExampleDirectory.FullName -ChildPath $FunctionName -Resolve
+if ($FunctionType = 'Private') {
+    [IO.FileInfo]  $TestFile             = Join-Path -Path $PrivateDirectory -ChildPath ($PesterFile.Name -replace '\.Tests\.', '.') -Resolve
+} else {
+    [IO.FileInfo]  $TestFile             = Join-Path -Path $PublicDirectory -ChildPath ($PesterFile.Name -replace '\.Tests\.', '.') -Resolve
+}
+
+. $TestFile
+# Get-ChildItem -Path $PublicDirectory -Filter '*.ps1' | Foreach-Object { . $_.FullName }
+
+
+
+[System.Collections.ArrayList] $Tests = @()
+$Examples = Get-ChildItem $ExampleDirectory -Filter "$($TestFile.BaseName).*.psd1" -File
+
+foreach ($Example in $Examples) {
+    [Hashtable] $Test = @{
+        Name = $Example.BaseName.Replace("$($TestFile.BaseName).$Verb", '').Replace('_', ' ')
+        Parameters = @{}
+    }
+    Write-Verbose "Test: $($Test | ConvertTo-Json)"
+
+    foreach ($ExampleData in (Import-PowerShellDataFile -LiteralPath $Example.FullName).GetEnumerator()) {
+        $Test.Parameters.Add($ExampleData.Name, $ExampleData.Value) | Out-Null
+    }
+
+    Write-Verbose "Test: $($Test | ConvertTo-Json)"
+    $Tests.Add($Test) | Out-Null
+}
+
+
+
+Describe $TestFile.Name {
+    Mock Get-ePOHelp {}
+    foreach ($Test in $Tests) {
+        Remove-Variable -Scope 'Script' -Name 'ePOwerShell' -Force -ErrorAction SilentlyContinue
+
+        Context $Test.Name {
+            [Hashtable] $Parameters = $Test.Parameters
+
+            It "ConvertTo-ePOComputer" {
+                { $Script:RequestResponse = ConvertTo-ePOComputer @Parameters } | Should Not Throw
+            }
+
+            It "Returns a string" {
+                $Script:RequestResponse.GetType().Fullname | Should Be 'ePOComputer'
+            }
+
+            Context "Has correct types" {
+                It 'Has the correct type of tags' {
+                    $Script:RequestResponse.Tags.GetType().FullName | Should Be 'System.String[]'
+                }
+
+                It 'Has the correct type for Agent GUID' {
+                    $Script:RequestResponse.AgentGUID.GetType().FullName | Should Be 'System.Guid'
+                }
+
+                It 'Has the correct type for Managed State' {
+                    $Script:RequestResponse.ManagedState.GetType().FullName | Should Be 'System.Boolean'
+                }
+            }
+        }
+    }
+}
--- ePOwerShell/Classes/ePOTable.ps1
+++ ePOwerShell/Classes/ePOTable.ps1
@@ -1,19 +1,19 @@
 Class ePOTable {
-    [System.String] $Name
-    [System.String] $Target
-    [System.String] $Type
-    [System.String] $DatabaseType
-    [System.String] $Description
-    [System.String[]] $RelatedTables
-    [ePOColumns]    $Columns
+    [System.String]     $Name
+    [System.String]     $Target
+    [System.String]     $Type
+    [System.String]     $DatabaseType
+    [System.String]     $Description
+    [System.String[]]   $RelatedTables
+    [ePOColumn[]]       $Columns
 }
 
-Class ePOColumns {
-    [System.String] $Name
-    [System.String] $Type
-    [System.Boolean] $Select
-    [System.Boolean] $Condition
-    [System.Boolean] $GroupBy
-    [System.Boolean] $Order
-    [System.Boolean] $Number
+Class ePOColumn {
+    [System.String]     $Name
+    [System.String]     $Type
+    [System.Boolean]    $Select
+    [System.Boolean]    $Condition
+    [System.Boolean]    $GroupBy
+    [System.Boolean]    $Order
+    [System.Boolean]    $Number
 }
