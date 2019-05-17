68113315572e6ae64917ee4bdbc2e452260ff771
Michael Arranaga <Michael.Arranaga@unt.edu>
2019-05-17 09:57:54 -0500

    Finished pester tests for Get-ePOQuery

--- Tests/Public/Get-ePOQuery.Tests.ps1
+++ Tests/Public/Get-ePOQuery.Tests.ps1
@@ -1,37 +1,59 @@
-[string]           $projectDirectoryName = 'ePOwerShell'
-[IO.FileInfo]      $pesterFile = [io.fileinfo] ([string] (Resolve-Path -Path $MyInvocation.MyCommand.Path))
-[IO.DirectoryInfo] $projectRoot = Split-Path -Parent $pesterFile.Directory
-[IO.DirectoryInfo] $projectDirectory = Join-Path -Path $projectRoot -ChildPath $projectDirectoryName -Resolve
-[IO.DirectoryInfo] $exampleDirectory = [IO.DirectoryInfo] ([String] (Resolve-Path (Get-ChildItem (Join-Path -Path $ProjectRoot -ChildPath 'Examples' -Resolve) -Filter (($pesterFile.Name).Split('.')[0]) -Directory).FullName))
-[IO.FileInfo]      $testFile = Join-Path -Path $projectDirectory -ChildPath (Join-Path -Path 'Public' -ChildPath ($pesterFile.Name -replace '\.Tests\.', '.')) -Resolve
-. $testFile
+[System.String]    $ProjectDirectoryName = 'ePOwerShell'
+[System.String]    $FunctionType         = 'Public'
+[IO.FileInfo]      $PesterFile           = [IO.FileInfo] ([System.String] (Resolve-Path -Path $MyInvocation.MyCommand.Path))
+[System.String]    $FunctionName         = $PesterFile.Name.Split('.')[0]
+[IO.DirectoryInfo] $ProjectRoot          = Split-Path -Parent $PesterFile.Directory
 
-. $(Join-Path -Path $projectDirectory -ChildPath (Join-Path -Path 'Private' -ChildPath 'Invoke-ePORequest.ps1') -Resolve)
-
-[System.Collections.ArrayList] $tests = @()
-$examples = Get-ChildItem $exampleDirectory -Filter "$($testFile.BaseName).*.psd1" -File
-
-foreach ($example in $examples) {
-    [hashtable] $test = @{
-        Name = $example.BaseName.Replace("$($testFile.BaseName).$verb", '').Replace('_', ' ')
-    }
-    Write-Verbose "Test: $($test | ConvertTo-Json)"
-
-    foreach ($exampleData in (Import-PowerShellDataFile -LiteralPath $example.FullName).GetEnumerator()) {
-        $test.Add($exampleData.Name, $exampleData.Value) | Out-Null
-    }
-
-    Write-Verbose "Test: $($test | ConvertTo-Json)"
-    $tests.Add($test) | Out-Null
+While (-not ($ProjectRoot.Name -eq $ProjectDirectoryName)) {
+    $ProjectRoot = Split-Path -Parent $ProjectRoot.FullName
 }
 
-Describe $testFile.Name {
-    foreach ($test in $tests) {
+[IO.DirectoryInfo] $ProjectDirectory     = Join-Path -Path $ProjectRoot -ChildPath $ProjectDirectoryName -Resolve
+[IO.DirectoryInfo] $PublicDirectory      = Join-Path -Path $ProjectDirectory -ChildPath 'Public' -Resolve 
+[IO.DirectoryInfo] $PrivateDirectory     = Join-Path -Path $ProjectDirectory -ChildPath 'Private' -Resolve 
+[IO.DirectoryInfo] $ClassesDirectory     = Join-Path -Path $ProjectDirectory -ChildPath 'Classes' -Resolve 
+[IO.DirectoryInfo] $ExampleDirectory     = Join-Path (Join-Path -Path $ProjectRoot -ChildPath 'Examples' -Resolve) -ChildPath $FunctionType -Resolve
+[IO.DirectoryInfo] $ExampleDirectory     = Join-Path $ExampleDirectory.FullName -ChildPath $FunctionName -Resolve
+[IO.DirectoryInfo] $ReferenceDirectory   = Join-Path $ExampleDirectory.FullName -ChildPath 'References' -Resolve
+if ($FunctionType -eq 'Private') {
+    [IO.FileInfo]  $TestFile             = Join-Path -Path $PrivateDirectory -ChildPath ($PesterFile.Name -replace '\.Tests\.', '.') -Resolve
+} else {
+    [IO.FileInfo]  $TestFile             = Join-Path -Path $PublicDirectory -ChildPath ($PesterFile.Name -replace '\.Tests\.', '.') -Resolve
+}
+
+. $TestFile
+Get-ChildItem -Path $PublicDirectory -Filter '*.ps1' | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $PrivateDirectory -Filter '*.ps1' | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $ClassesDirectory -Filter '*.ps1' | ForEach-Object { . $_.FullName }
+
+
+
+[System.Collections.ArrayList] $Tests = @()
+$Examples = Get-ChildItem $ExampleDirectory -Filter "$($TestFile.BaseName).*.psd1" -File
+
+foreach ($Example in $Examples) {
+    [hashtable] $Test = @{
+        Name = $Example.BaseName.Replace("$($TestFile.BaseName).$verb", '').Replace('_', ' ')
+    }
+    Write-Verbose "Test: $($Test | ConvertTo-Json)"
+
+    foreach ($ExampleData in (Import-PowerShellDataFile -LiteralPath $Example.FullName).GetEnumerator()) {
+        $Test.Add($ExampleData.Name, $ExampleData.Value) | Out-Null
+    }
+
+    Write-Verbose "Test: $($Test | ConvertTo-Json)"
+    $Tests.Add($Test) | Out-Null
+}
+
+
+
+Describe $TestFile.Name {
+    foreach ($Test in $Tests) {
         Mock Invoke-ePORequest {
-            if ($test.Output.Throws) {
+            if ($Test.Output.Throws) {
                 return $Null
             } else {
-                $File = Get-ChildItem (Join-Path -Path $exampleDirectory -ChildPath 'References' -Resolve) -Filter 'Queries.html'
+                $File = Get-ChildItem $ReferenceDirectory -Filter 'Queries.html'
             }
 
             return (Get-Content $File.FullName | Out-String).Substring(3).Trim() | ConvertFrom-Json
@@ -39,7 +61,7 @@ Describe $testFile.Name {
 
         Remove-Variable -Scope 'Script' -Name 'RequestResponse' -Force -ErrorAction SilentlyContinue
 
-        Context $test.Name {
+        Context $Test.Name {
             if ($Test.Output.Throws) {
                 It "Get-ePOwerShellQueries Throws" {
                     { $script:RequestResponse = Get-ePOwerShellQueries } | Should Throw
@@ -50,12 +72,18 @@ Describe $testFile.Name {
             It "Get-ePOwerShellQueries" {
                 { $script:RequestResponse = Get-ePOwerShellQueries } | Should Not Throw
             }
-            
-            It "Output Type: $($test.Output.Type)" {
-                if ($test.Output.Type -eq 'System.Void') {
+
+            It "Output Type: $($Test.Output.Type)" {
+                if ($Test.Output.Type -eq 'System.Void') {
                     $script:RequestResponse | Should BeNullOrEmpty
                 } else {
-                    $script:RequestResponse.GetType().FullName | Should Be $test.Output.Type
+                    $script:RequestResponse.GetType().FullName | Should Be $Test.Output.Type
+                }
+            }
+
+            foreach ($Query in $script:RequestResponse) {
+                It "Item is an ePOQuery object" {
+                    $Query.GetType().FullName | Should Be 'ePOQuery'
                 }
             }
         }
